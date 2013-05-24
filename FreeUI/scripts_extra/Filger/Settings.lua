e494b3b324bf6f7fcc91ab52e8f20c47d2f843df
Matt G <sleeve.dev@Gmail.com>
2013-05-25 03:33:25 +0800

    Revert "update"
    This reverts commit 4c86446d480ebd0924c5847bf29a757dbc55f397.

--- FreeUI/scripts_extra/Filger/Settings.lua
+++ FreeUI/scripts_extra/Filger/Settings.lua
@@ -1,56 +1,43 @@
--- 显示法术ID和来源：取自SpellID插件并改进
-local fontcolor = { r=0.44, g=0.83, b=1 }
-local SPELLID, FROM
+﻿-- 设置
+local Misc = CreateFrame("Frame")
+local Media = "Interface\\AddOns\\FreeUI\\scripts_extra\\Filger\\Media\\"
+	Misc.Media = Media
 
-if GetLocale() == "zhCN" then
-	SPELLID = "法术编号："
-	FROM = "源自："
-elseif GetLocale() == "zhTW" then
-	SPELLID = "法術編號："
-	FROM = "源自："
-else
-	SPELLID = "SpellID: "
-	FROM = " from: "
-end
+	-- Filger.lua
+	Misc.font = Media.."Pixel.ttf"	-- 所有数字的字体
+	Misc.barfg = Media.."barfg"		-- 计时条材质
+	Misc.modefg = "OWN"			-- 计时条颜色
+	Misc.modeback = "Black"		-- 图标背景或蒙板颜色（一像素和毛绒绒样式无效）
+	Misc.modeborder = "Black"	-- 边框颜色
+	Misc.numsize = 14			-- 层数, 计时条的计时数字大小
+	Misc.namesize = 14			-- 法术名称字体大小
 
-local function UnitToColorText(unit)
-	local ClassColor = RAID_CLASS_COLORS[select(2,UnitClass(unit))] or fontcolor;
-	if (not UnitIsPlayer(unit)) then ClassColor = fontcolor end;
-	return format("|cFF%s%s|r",format("%02X%02X%02X",ClassColor.r*255,ClassColor.g*255,ClassColor.b*255),UnitName(unit));
-end
+	-- Cooldowns.lua
+	Misc.cdsize = 16			-- 图标中间的 CD 数字大小
 
-hooksecurefunc(GameTooltip, "SetUnitBuff", function(self,...)
-	local _,_,_,_,_,_,_,unitCaster,_,_,id = UnitBuff(...);
-	if id then
-		if unitCaster then
-			self:AddDoubleLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r","|cFF00FF00Buff"..FROM..UnitToColorText(unitCaster));
-		else
-			self:AddLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r");
-		end
-		self:Show();
-	end
-end)
+	-- config.lua
+	Misc.Tbar = "ON"			-- 开(ON)\关(OFF) target_bar 目标计时条
+	Misc.Pbar = "ON"			-- 开(ON)\关(OFF) pve_cc 计时条
+	Misc.barw = 180				-- 计时条长度
+	Misc.CDnum = 6				-- COOLDOWN 冷却图标每行显示几个
 
-hooksecurefunc(GameTooltip, "SetUnitDebuff", function(self,...)
-	local _,_,_,_,_,_,_,unitCaster,_,_,id = UnitDebuff(...);
-	if id then
-		if unitCaster then
-			self:AddDoubleLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r","|cFF00FF00Debuff"..FROM..UnitToColorText(unitCaster));
-		else
-			self:AddLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r");
-		end
-		self:Show();
-	end
-end)
+-------------------------------------------------------- 
+getscreenheight = tonumber(string.match(({GetScreenResolutions()})[GetCurrentResolution()], "%d+x(%d+)")) 
+getscreenwidth = tonumber(string.match(({GetScreenResolutions()})[GetCurrentResolution()], "(%d+)x+%d")) 
 
-hooksecurefunc(GameTooltip, "SetUnitAura", function(self,...)
-	local _,_,_,_,_,_,_,unitCaster,_,_,id = UnitAura(...);
-	if id then
-		if unitCaster then
-			self:AddDoubleLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r","|cFF00FF00Aura"..FROM..UnitToColorText(unitCaster));
-		else
-			self:AddLine("|cFF00FF00"..SPELLID.."|cFF71D5FF"..id.."|r");
-		end
-		self:Show();
-	end
-end)
+--   Pixel perfect script of custom ui Scale 
+UIScale = function() 
+   uiscale = min(2, max(0.64, 768 / string.match(GetCVar("gxResolution"), "%d+x(%d+)"))) 
+end 
+UIScale() 
+
+local mult = 768 / string.match(GetCVar("gxResolution"), "%d+x(%d+)") / uiscale 
+local Scale = function(x) 
+   return mult * math.floor(x / mult + 0.5) 
+end 
+Misc.mult = mult 
+----------------------- ShestakUI_Filger_Fluffy -----------------------
+
+-- 命名空间
+local _, sakaras = ...
+sakaras.FilgerSettings = Misc
