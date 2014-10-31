5a72c9ed69da1c9726a1ee8acc99165c4e7e52a3
Matt G <sleeve.dev@Gmail.com>
2014-11-01 08:46:05 +1100

    rObjectiveTrackerMover by zork

--- FreeUI/scripts/objectivetracker.lua
+++ FreeUI/scripts/objectivetracker.lua
@@ -1,150 +1,46 @@
-local F, C, L = unpack(select(2, ...))
 
-local ot = ObjectiveTrackerFrame
-local BlocksFrame = ot.BlocksFrame
+  -- // rObjectiveTrackerMover
+  -- // zork - 2014
 
--- [[ Positioning ]]
+  -----------------------------
+  -- VARIABLES
+  -----------------------------
+  
+  local an, at = ...
+  local unpack = unpack  
+  local ObjectiveTrackerFrame = ObjectiveTrackerFrame
 
-local function moveTracker()
-	local xCoord, yAnchor
+  local frame = CreateFrame("Frame")
+  
+  local cfg = {}
+  cfg.y = -20
+ 
+  -----------------------------
+  -- FUNCTIONS
+  -----------------------------
 
-	if MultiBarLeft:IsShown() then
-		xCoord = -84
-	elseif MultiBarRight:IsShown() then
-		xCoord = -57
-	else
-		xCoord = -30
-	end
+  local function AdjustSetPoint(self,...)
+    local a1,af,a2,x,y = ...
+    if a1 and af == "MinimapCluster" and y ~= cfg.y then    
+      if not InCombatLockdown() then
+        self:SetPoint(a1,af,a2,x,cfg.y)
+      else
+        frame.point = {a1,af,a2,x,cfg.y}
+        frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+      end      
+    end
+  end 
 
-	yAnchor = VehicleSeatIndicator:IsShown() and VehicleSeatIndicator or Minimap
+  frame:SetScript("OnEvent", function(self,event)
+    self:UnregisterEvent(event)
+    if event == "PLAYER_LOGIN" then
+      self.point = {ObjectiveTrackerFrame:GetPoint()}      
+      hooksecurefunc(ObjectiveTrackerFrame, "SetPoint", AdjustSetPoint)    
+    end
+    if not InCombatLockdown() then
+      ObjectiveTrackerFrame:SetPoint(unpack(self.point))
+    end
+  end)
+  
+  frame:RegisterEvent("PLAYER_LOGIN")
 
-	ot:ClearAllPoints()
-	ot:SetPoint("TOPRIGHT", UIParent, "TOPRIGHT", xCoord, -160)
-	ot:SetPoint("TOP", yAnchor, "BOTTOM", 0, -30) -- bogus positioning because we can't touch ObjectiveTracker_CanFitBlock
-end
-
-hooksecurefunc(ot, "SetPoint", function(_, _, _, point)
-	if point == "BOTTOMRIGHT" then
-		moveTracker()
-	end
-end)
-
-hooksecurefunc(VehicleSeatIndicator, "SetPoint", function(_, _, anchor)
-	if anchor ~= Minimap then
-		VehicleSeatIndicator:ClearAllPoints()
-		VehicleSeatIndicator:SetPoint("BOTTOM", Minimap, "TOP", 0, 20)
-	end
-end)
-
--- [[ Header ]]
-
--- Header
-
-F.SetFS(ot.HeaderMenu.Title)
-
--- Minimize button
-
-local minimizeButton = ot.HeaderMenu.MinimizeButton
-
-F.ReskinExpandOrCollapse(minimizeButton)
-minimizeButton:SetSize(15, 15)
-minimizeButton.plus:Hide()
-
-hooksecurefunc("ObjectiveTracker_Collapse", function()
-	minimizeButton.plus:Show()
-end)
-hooksecurefunc("ObjectiveTracker_Expand", function()
-	minimizeButton.plus:Hide()
-end)
-
--- [[ Blocks and lines ]]
-
-for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"}) do
-	local header = BlocksFrame[headerName]
-
-	header.Background:Hide()
-	F.SetFS(header.Text)
-end
-
-hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-	if not block.headerStyled then
-		F.SetFS(block.HeaderText)
-		block.headerStyled = true
-	end
-end)
-
-hooksecurefunc(QUEST_TRACKER_MODULE, "SetBlockHeader", function(_, block)
-	if not block.headerStyled then
-		F.SetFS(block.HeaderText)
-		block.headerStyled = true
-	end
-
-	local itemButton = block.itemButton
-
-	if itemButton and not itemButton.styled then
-		itemButton:SetNormalTexture("")
-		itemButton:SetPushedTexture("")
-
-		itemButton.HotKey:ClearAllPoints()
-		itemButton.HotKey:SetPoint("TOP", itemButton, -1, 0)
-		itemButton.HotKey:SetJustifyH("CENTER")
-		F.SetFS(itemButton.HotKey)
-
-		itemButton.icon:SetTexCoord(.08, .92, .08, .92)
-		F.CreateBG(itemButton)
-
-		itemButton.styled = true
-	end
-end)
-
-hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "AddObjective", function(self, block)
-	if block.module == QUEST_TRACKER_MODULE or block.module == ACHIEVEMENT_TRACKER_MODULE then
-		local line = block.currentLine
-
-		local p1, a, p2, x, y = line:GetPoint()
-		line:SetPoint(p1, a, p2, x, y - 4)
-	end
-end)
-
-local function fixBlockHeight(block)
-	if block.shouldFix then
-		local height = block:GetHeight()
-
-		if block.lines then
-			for _, line in pairs(block.lines) do
-				if line:IsShown() then
-					height = height + 4
-				end
-			end
-		end
-
-		block.shouldFix = false
-		block:SetHeight(height + 5)
-		block.shouldFix = true
-	end
-end
-
-hooksecurefunc("ObjectiveTracker_AddBlock", function(block)
-	if block.lines then
-		for _, line in pairs(block.lines) do
-			if not line.styled then
-				F.SetFS(line.Text)
-				line.Text:SetSpacing(2)
-
-				if line.Dash then
-					F.SetFS(line.Dash)
-				end
-
-				line:SetHeight(line.Text:GetHeight())
-
-				line.styled = true
-			end
-		end
-	end
-
-	if not block.styled then
-		block.shouldFix = true
-		hooksecurefunc(block, "SetHeight", fixBlockHeight)
-		block.styled = true
-	end
-end)
