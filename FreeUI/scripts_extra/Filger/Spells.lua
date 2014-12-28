d4ec8bd26297d3d966e777ceba980b80db9eb167
solor <Nyctophilia.A@outlook.com>
2014-12-29 01:36:19 +0800

    update filger for DK

--- FreeUI/scripts_extra/Filger/Spells.lua
+++ FreeUI/scripts_extra/Filger/Spells.lua
@@ -167,6 +167,167 @@ Filger_Spells = {
 			{slotID = 14, filter = "CD"},
 		},
 	},
+	["DEATHKNIGHT"] = {
+		{
+			Name = "P_AURA",
+			Direction = Misc.Dright,
+			Mode = "ICON",
+			Interval = 0,
+			Alpha = 1,
+			IconSize = Misc.IconSizeNormal,
+			Position = {unpack(Filger_Settings.P_AURA)},
+
+			-- talent
+
+			-- Bone Shield
+			{spellID = 49222, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Vampiric Blood
+			{spellID = 55233, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Icebound Fortitude
+			{spellID = 48792, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Anti-Magic Shell
+			{spellID = 48707, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Dancing Rune Weapon
+			{spellID = 81256, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Lichborne
+			{spellID = 49039, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Pillar of Frost
+			{spellID = 51271, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Death's Advance
+			{spellID = 96268, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Unholy Blight
+			{spellID = 115989, unitID = "player", caster = "player", filter = "BUFF"},
+
+			-- Dark Succor
+			{spellID = 101568, unitID = "player", caster = "player", filter = "BUFF"},
+
+			-- Scent of Blood
+			{spellID = 50421, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Crimson Scourge
+			{spellID = 81141, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Freezing Fog
+			{spellID = 59052, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Killing Machine
+			{spellID = 51124, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Sudden Doom
+			{spellID = 81340, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Unholy Strength
+			{spellID = 53365, unitID = "player", caster = "player", filter = "BUFF"},
+			-- Dark Transformation
+			{spellID = 63560, unitID = "pet", caster = "player", filter = "BUFF"},
+			-- Shadow Infusion
+			{spellID = 91342, unitID = "pet", caster = "player", filter = "BUFF", count = 5},
+			-- Blood Charge
+			{spellID = 114851, unitID = "player", caster = "player", filter = "BUFF"},
+		},
+		{
+			Name = "T_AURA_P",
+			Direction = Misc.Dright,
+			Mode = "ICON",
+			Interval = 0,
+			Alpha = 1,
+			IconSize = Misc.IconSizeNormal,
+			Position = {unpack(Filger_Settings.T_AURA_P)},
+
+			-- Blood Plague
+			{spellID = 55078, unitID = "target", caster = "player", filter = "DEBUFF"},
+			-- Frost Fever
+			{spellID = 55095, unitID = "target", caster = "player", filter = "DEBUFF"},
+		},
+		{
+			Name = "P_CD",
+			Direction = Misc.Dleft,
+			Mode = "ICON",
+			NumPerLine = Misc.CDnum,
+			Interval = 0,
+			Alpha = 1,
+			IconSize = Misc.IconSizeNormal,
+			Position = {unpack(Filger_Settings.P_CD)},
+
+			-- Self
+			-- Mind Freeze
+			{spellID = 47528, filter = "CD"},
+			-- Strangulate
+			{spellID = 47476, filter = "CD"},
+			-- Gnaw (Ghoul)
+			{spellID = 47481, filter = "CD"},
+			-- Remorseless Winter
+			{spellID = 108200, filter = "CD"},
+			-- Dark Simulacrum
+			{spellID = 77606, filter = "CD"},
+			-- Soul Reaper
+			{spellID = 130736, filter = "CD"},
+			-- Death Grip
+			{spellID = 49576, filter = "CD"},
+			-- Plague Leech
+			{spellID = 123693, filter = "CD"},
+			-- Death and Decay
+			{spellID = 43265, filter = "CD"},
+			-- Death's Advance
+			{spellID = 96268, filter = "CD"},
+			-- Rune Tap
+			{spellID = 48982, filter = "CD"},
+			-- Anti-Magic Shell
+			{spellID = 48707, filter = "CD"},
+			-- Vampiric Blood
+			{spellID = 55233, filter = "CD"},
+			-- Pillar of Frost
+			{spellID = 51271, filter = "CD"},
+			-- Outbreak
+			{spellID = 77575, filter = "CD"},
+			-- Gorefiend's Grasp
+			{spellID = 108199, filter = "CD"},
+			-- Raise Dead
+			{spellID = 46584, filter = "CD"},
+			-- Dancing Rune Weapon
+			{spellID = 49028, filter = "CD"},
+			-- Unholy Blight
+			{spellID = 115989, filter = "CD"},
+			-- Lichborne
+			{spellID = 49039, filter = "CD"},
+			-- Desecrated Ground
+			{spellID = 108201, filter = "CD"},
+			-- Death Pact
+			{spellID = 48743, filter = "CD"},
+			-- Anti-Magic Zone
+			{spellID = 51052, filter = "CD"},
+			-- Icebound Fortitude
+			{spellID = 48792, filter = "CD"},
+			-- Summon Gargoyle
+			{spellID = 49206, filter = "CD"},
+
+			-- Racial
+			-- Every Man for Himself (Human)
+			{spellID = 59752, filter = "CD"},
+			-- Stoneform (Dwarf)
+			{spellID = 20594, filter = "CD"},
+			-- Escape Artist (Gnome)
+			{spellID = 20589, filter = "CD"},
+			-- Darkflight (Worgen)
+			{spellID = 68992, filter = "CD"},
+			-- Blood Fury (Orc)
+			{spellID = 20572, filter = "CD"},
+			-- Will of the Forsaken (Forsaken)
+			{spellID = 7744, filter = "CD"},
+			-- Cannibalize (Forsaken)
+			{spellID = 20577, filter = "CD"},
+			-- Berserking (Troll)
+			{spellID = 26297, filter = "CD"},
+			-- Arcane Torrent (Blood Elf)
+			{spellID = 69179, filter = "CD"},
+			-- Rocket Jump (Goblin)
+			{spellID = 69070, filter = "CD"},
+
+			-- Items
+			-- Belt
+			{slotID = 6, filter = "CD"},
+			-- Back
+			{slotID = 15, filter = "CD"},
+			-- Trinkets
+			{slotID = 13, filter = "CD"},
+			{slotID = 14, filter = "CD"},
+		},
+	},
 
 	["ALL"] = {			
 		{
