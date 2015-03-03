bfdf8fe8ad101d1e9b65b44fd5d37bd6a90c0973
solor <Nyctophilia.A@outlook.com>
2015-03-03 16:20:29 +0800

    play different sound on whisper/real ID messages

--- FreeUI/scripts_extra/WhisperSound.lua
+++ FreeUI/scripts_extra/WhisperSound.lua
@@ -1,12 +1,32 @@
-----------------------------------------------------------------------------------------
---	Play sound files system(by Tukz)
-----------------------------------------------------------------------------------------
-local SoundSys = CreateFrame("Frame")
-SoundSys:RegisterEvent("CHAT_MSG_WHISPER")
-SoundSys:RegisterEvent("CHAT_MSG_BN_WHISPER")
-SoundSys:HookScript("OnEvent", function(self, event, msg, ...)
-	if event == "CHAT_MSG_WHISPER" or event == "CHAT_MSG_BN_WHISPER" then
-		if (msg:sub(1,3) == "OQ,") then return false, msg, ... end
-		PlaySoundFile("Interface\\AddOns\\FreeUI\\media\\Whisper.ogg", "Master")
-	end
+
+local sounds = {
+	["CHAT_MSG_WHISPER"] = "Interface\\AddOns\\FreeUI\\media\\whisper.ogg", MASTER,
+	["CHAT_MSG_BN_WHISPER"] = "Interface\\AddOns\\FreeUI\\media\\whisper2.ogg", MASTER,
+	["CHAT_MSG_BN_CONVERSATION"] = "Interface\\AddOns\\FreeUI\\media\\whisper2.ogg", MASTER,
+--	["CHAT_MSG_PARTY"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_PARTY_LEADER"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_PARTY_GUIDE"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_OFFICER"] = "Interface\\AddOns\\FreeUI\\media\\guild.ogg", MASTER,
+--	["CHAT_MSG_GUILD"] = "Interface\\AddOns\\FreeUI\\media\\guild.ogg", MASTER,
+--  ["CHAT_MSG_RAID"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_RAID_LEADER"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_INSTANCE_CHAT"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+--	["CHAT_MSG_INSTANCE_CHAT_LEADER"] = "Interface\\AddOns\\FreeUI\\media\\party.ogg", MASTER,
+}
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("CHAT_MSG_WHISPER")
+f:RegisterEvent("CHAT_MSG_BN_WHISPER")
+f:RegisterEvent("CHAT_MSG_BN_CONVERSATION")
+-- f:RegisterEvent("CHAT_MSG_RAID_LEADER")
+-- f:RegisterEvent("CHAT_MSG_RAID")
+-- f:RegisterEvent("CHAT_MSG_PARTY")
+-- f:RegisterEvent("CHAT_MSG_OFFICER")
+-- f:RegisterEvent("CHAT_MSG_GUILD")
+-- f:RegisterEvent("CHAT_MSG_PARTY_LEADER")
+-- f:RegisterEvent("CHAT_MSG_PARTY_GUIDE")
+-- f:RegisterEvent("CHAT_MSG_INSTANCE_CHAT")
+-- f:RegisterEvent("CHAT_MSG_INSTANCE_CHAT_LEADER")
+f:SetScript("OnEvent", function(self, event)
+	PlaySoundFile(sounds[event])
 end)
