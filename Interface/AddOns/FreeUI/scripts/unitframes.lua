c236255b1036b48bac631b73fac7ca131c4730c7
solor <naushika@outlook.com>
2015-09-13 18:05:24 +1000

    shift + 左键设焦点可以对 ouf 的小队和团队使用了

--- Interface/AddOns/FreeUI/scripts/unitframes.lua
+++ Interface/AddOns/FreeUI/scripts/unitframes.lua
@@ -352,20 +352,6 @@ end
 
 
 
---[[ set/clear focus with shift + left click ]]
-local focMod = 'shift-'
-local focBut = '1'
-hooksecurefunc("CompactUnitFrame_SetUpFrame", function(frame, ...)
-	if frame then
-		frame:SetAttribute(focMod.."type"..focBut, "focus")
-	end
-end)
-local foc = CreateFrame("CheckButton", "Focuser", UIParent, "SecureActionButtonTemplate")
-foc:SetAttribute("type1", "macro")
-foc:SetAttribute("macrotext", "/focus mouseover")
-SetOverrideBindingClick(Focuser, true, focMod.."BUTTON"..focBut, "Focuser")
-
-
 --[[ Debuff highlight ]]
 local PostUpdateIcon = function(_, unit, icon, index, _, filter)
 	local _, _, _, _, dtype = UnitAura(unit, index, icon.filter)
@@ -434,6 +420,40 @@ local Shared = function(self, unit, isSingle)
 
 	self.bd = bd
 
+	--[[ set/clear focus by shift + left click ]]
+
+	local modifier = "shift" -- shift, alt or ctrl
+	local mouseButton = "1" -- 1 = left, 2 = right, 3 = middle, 4 and 5 = thumb buttons if there are any
+
+	local function SetFocusHotkey(frame)
+		frame:SetAttribute(modifier.."-type"..mouseButton,"focus")
+	end
+
+	local function CreateFrame_Hook(type, name, parent, template)
+		if template == "SecureUnitButtonTemplate" then
+			SetFocusHotkey(_G[name])
+		end
+	end
+
+	hooksecurefunc("CreateFrame", CreateFrame_Hook)
+
+	-- Keybinding override so that models can be shift/alt/ctrl+clicked
+	local f = CreateFrame("CheckButton", "FocuserButton", UIParent, "SecureActionButtonTemplate")
+	f:SetAttribute("type1","macro")
+	f:SetAttribute("macrotext","/focus mouseover")
+	SetOverrideBindingClick(FocuserButton,true,modifier.."-BUTTON"..mouseButton,"FocuserButton")
+
+	-- for ouf unit
+	-- I really dont know why
+	local ModKey = "shift"
+    local MouseButton = 1
+    local key = ModKey .. "-type" .. (MouseButton or "")
+    if(self.unit == "focus") then
+        self:SetAttribute(key, "macro")
+        self:SetAttribute("macrotext", "/clearfocus")
+    else
+        self:SetAttribute(key, "focus")
+    end
 
 	--[[ Health ]]
 
