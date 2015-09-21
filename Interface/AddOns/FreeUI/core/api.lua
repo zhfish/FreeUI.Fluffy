c7bc619ce1b8f324b5c4b0cbd1e4a388c7c19e98
solor <naushika@outlook.com>
2015-09-22 05:56:52 +1000

    change reputation bar colors to the newer battle.net ones

--- Interface/AddOns/FreeUI/core/api.lua
+++ Interface/AddOns/FreeUI/core/api.lua
@@ -51,6 +51,17 @@ C.classcolours = {
 	["WARRIOR"] = {r = 0.79, g = 0.61, b = 0.5},
 }
 
+FACTION_BAR_COLORS = {
+	[1] = {r = 0.63, g = 0, b = 0},
+	[2] = {r = 0.63, g = 0, b = 0},
+	[3] = {r = 0.63, g = 0, b = 0},
+	[4] = {r = 0.82, g = 0.67, b = 0},
+	[5] = {r = 0.32, g = 0.67, b = 0},
+	[6] = {r = 0.32, g = 0.67, b = 0},
+	[7] = {r = 0.32, g = 0.67, b = 0},
+	[8] = {r = 0, g = 0.75, b = 0.44},
+};
+
 local _, class = UnitClass("player")
 if C.appearance.colourScheme == 2 then
 	C.class = {C.appearance.customColour.r, C.appearance.customColour.g, C.appearance.customColour.b}
