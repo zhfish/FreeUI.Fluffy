8ae56e50469d1cc317bc3700cb6fac93f13a2f2c
solor <Naushika@Outlook.com>
2016-07-30 19:10:04 +0800

    Revert "add a pixel font for Chinese text"
    This reverts commit 066965c965a9b7ed8be84da5f8e502997d6d995a.

--- Interface/AddOns/FreeUI/scripts/extra/myMedia.lua
+++ Interface/AddOns/FreeUI/scripts/extra/myMedia.lua
@@ -1,5 +1,4 @@
 local LSM = _G.LibStub("LibSharedMedia-3.0")
-local koKR, ruRU, zhCN, zhTW, western = LSM.LOCALE_BIT_koKR, LSM.LOCALE_BIT_ruRU, LSM.LOCALE_BIT_zhCN, LSM.LOCALE_BIT_zhTW, LSM.LOCALE_BIT_western
 -- -----
 -- BACKGROUND
 -- -----
@@ -21,8 +20,6 @@ LSM:Register("font", "swfit", 							[[Interface\Addons\FreeUI\media\swfit.ttf]]
 LSM:Register("font", "PFTempestaSeven", 				[[Interface\Addons\FreeUI\media\pixel.ttf]])
 LSM:Register("font", "myriad semibold", 				[[Interface\Addons\FreeUI\media\font.ttf]])
 LSM:Register("font", "wendy_10", 						[[Interface\Addons\FreeUI\media\wendy_10.ttf]])
-
-LSM:Register("font", "pixfontCN", 						[[Fonts\pixfontCN.ttf]], zhCN + zhTW + western)
 -- -----
 --   SOUND
 -- -----
