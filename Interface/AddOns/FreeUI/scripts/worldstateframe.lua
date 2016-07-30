540a1b7d2a6bfb84cc3912e693754c4af4ba09e1
solor <Naushika@Outlook.com>
2016-07-30 18:58:50 +0800

    修正战场资源信息不显示中文的问题

--- Interface/AddOns/FreeUI/scripts/worldstateframe.lua
+++ Interface/AddOns/FreeUI/scripts/worldstateframe.lua
@@ -1,11 +1,18 @@
 local F, C = unpack(select(2, ...))
 
+local locale = GetLocale()
+
 local function restyleStateFrames()
 	for i = 1, NUM_ALWAYS_UP_UI_FRAMES do
 		local f = _G["AlwaysUpFrame"..i]
 		if f and not f.styled then
 			local _, g = f:GetRegions()
 			F.SetFS(g)
+
+			if locale == "zhCN" or locale == "zhTW" then
+				g:SetFont(C.media.font3, 10, "OUTLINEMONOCHROME")
+			end
+
 			g:SetShadowOffset(0, 0)
 			g:SetTextColor(1, 1, 1)
 
