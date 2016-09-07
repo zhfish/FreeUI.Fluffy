ec194c3d131e76371f8e9a6651e3cd6b67c2a289
solor <Naushika@Outlook.com>
2016-09-08 06:39:51 +0800

    carg_Bags_Nivaya 统一背包每行10个，银行每行14个

--- Interface/AddOns/cargBags_Nivaya/config.lua
+++ Interface/AddOns/cargBags_Nivaya/config.lua
@@ -5,12 +5,12 @@ itemSlotSize = 32,	-- Size of item slots
 
 sizes = {
 	bags = {
-		columnsSmall = 8,
+		columnsSmall = 10,
 		columnsLarge = 10,
 		largeItemCount = 64,	-- Switch to columnsLarge when >= this number of items in your bags
 	},
 	bank = {
-		columnsSmall = 12,
+		columnsSmall = 14,
 		columnsLarge = 14,
 		largeItemCount = 96,	-- Switch to columnsLarge when >= this number of items in the bank
 	},	
