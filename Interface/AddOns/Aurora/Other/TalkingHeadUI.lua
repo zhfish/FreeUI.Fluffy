8fa32f629ce12271fdd70f9e2722d33129a5419e
solor <Naushika@Outlook.com>
2016-09-13 09:33:32 +0800

    update talking head

--- Interface/AddOns/Aurora/Other/TalkingHeadUI.lua
+++ Interface/AddOns/Aurora/Other/TalkingHeadUI.lua
@@ -13,15 +13,56 @@ local F, C = _G.unpack(private.Aurora)
 
 C.themes["Blizzard_TalkingHeadUI"] = function()
 
-	local frame = CreateFrame("Frame", UIParent)
-	frame:SetScale(.6)
-	frame:SetPoint("TOP", 0, -100)
-	frame:SetSize(64,64)
+	-- TalkingHeadFrame = F.dummy
+	-- TalkingHeadFrame.MainFrame = F.dummy
+	-- TalkingHeadFrame.PortraitFrame = F.dummy
+	TalkingHeadFrame.MainFrame.Model.PortraitBg:Hide()
+	
+	F.ReskinClose(TalkingHeadFrame.MainFrame.CloseButton)
+	TalkingHeadFrame.MainFrame.CloseButton:Hide()
 
+	local scale = .8
 
-	TalkingHeadFrame:SetParent(frame)
-	TalkingHeadFrame:ClearAllPoints()
-	TalkingHeadFrame:SetPoint("CENTER")
+	--Sanitize
+	if scale < 0.5 then	scale = 0.5
+	elseif scale > 2 then scale = 2	end
+
+	--:SetScale no longer triggers OnSizeChanged in Legion, and as such the mover will not update its size
+	--Calculate dirtyWidth/dirtyHeight based on original size and scale
+	--This way the mover frame will use the right size when we manually trigger "OnSizeChanged"
+	local width = TalkingHeadFrame:GetWidth() * scale
+	local height = TalkingHeadFrame:GetHeight() * scale
+	TalkingHeadFrame.dirtyWidth = width
+	TalkingHeadFrame.dirtyHeight = height
+
+	TalkingHeadFrame:SetScale(scale)
+
+	--Reset Model Camera
+	-- local model = TalkingHeadFrame.MainFrame.Model
+	-- if model.uiCameraID then
+	-- 	model:RefreshCamera()
+	-- 	Model_ApplyUICamera(model, model.uiCameraID)
+	-- end
+
+	--Use this to prevent the frame from auto closing, so you have time to test things.
+	-- TalkingHeadFrame:UnregisterEvent("SOUNDKIT_FINISHED")
+	-- TalkingHeadFrame:UnregisterEvent("TALKINGHEAD_CLOSE")
+	-- TalkingHeadFrame:UnregisterEvent("LOADING_SCREEN_ENABLED")
+
+	--Prevent WoW from moving the frame around
 	TalkingHeadFrame.ignoreFramePositionManager = true
+	UIPARENT_MANAGED_FRAME_POSITIONS["TalkingHeadFrame"] = nil
+
+	--Set default position
+	TalkingHeadFrame:ClearAllPoints()
+	TalkingHeadFrame:SetPoint("TOP", 0, -100)
+	
+	--Iterate through all alert subsystems in order to find the one created for TalkingHeadFrame, and then remove it.
+	--We do this to prevent alerts from anchoring to this frame when it is shown.
+	for index, alertFrameSubSystem in ipairs(AlertFrame.alertFrameSubSystems) do
+		if alertFrameSubSystem.anchorFrame and alertFrameSubSystem.anchorFrame == TalkingHeadFrame then
+			table.remove(AlertFrame.alertFrameSubSystems, index)
+		end
+	end
 
 end
