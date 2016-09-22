5fdcba4816832a7a762a2afd4c4a79df7b4be86e
solor <Naushika@outlook.com>
2016-09-23 05:13:45 +0800

    update Aurora

--- Interface/AddOns/Aurora/AddOns/Blizzard_ChallengesUI.lua
+++ Interface/AddOns/Aurora/AddOns/Blizzard_ChallengesUI.lua
@@ -9,4 +9,23 @@ local _, C = _G.unpack(private.Aurora)
 C.themes["Blizzard_ChallengesUI"] = function()
 	_G.ChallengesFrameInset:DisableDrawLayer("BORDER")
 	_G.ChallengesFrameInsetBg:Hide()
+
+	_G.ChallengesModeWeeklyBest.Child.Level:SetPoint("CENTER", ChallengesModeWeeklyBest.Child.Star, "CENTER", 0, 3)
+		
+	select(1, _G.ChallengesFrame.GuildBest:GetRegions()):Hide()
+	select(3, _G.ChallengesFrame.GuildBest:GetRegions()):Hide()
+	
+	for i = 1, 2 do
+		select(i, _G.ChallengesFrame:GetRegions()):Hide()
+	end
+	
+	hooksecurefunc("ChallengesFrame_Update", function()
+		for i = 1, 9 do
+			local bu = _G.ChallengesFrame.DungeonIcons[i]
+			if bu then
+				select(1, bu:GetRegions()):Hide()
+				bu.Icon:SetTexCoord(.08, .92, .08, .92)
+			end
+		end
+	end)
 end
